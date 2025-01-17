00ced5dd2bb64ab91e6f792ed9ba84f1cd877d4f
Mehdi <149390266+mehdihz2003@users.noreply.github.com>
2025-01-17 10:29:42 +0100

    Properties and methods added
    Minimum properties from assignment added. Add more when needed

--- js/Product.js
+++ js/Product.js
@@ -1,6 +1,40 @@
-class Product{
+export class Product{
+    #title;
+    #imageURL;
+    #stock;
+    #price;
+    #discount;
+    #category;
+    #rating;
+
     constructor(data) {
-        this.data = data;
+        this.#title = data.title;
+        this.#imageURL = data.images[0];
+        this.#stock = data.stock;
+        this.#price = data.price;
+        this.#discount = data.discountPercentage;
+        this.#category = data.category;
+        this.#rating = data.rating;
+    }
+    
+    GetData() {
+        return {
+            title: this.#title,
+            imageURL: this.#imageURL,
+            stock: this.#stock,
+            price: this.#price,
+            discount: this.#discount,
+            category: this.#category,
+            rating: this.#rating,
+        };
+    }
+
+    #calcDiscount() {
+        this.#price = this.#price * (1 - this.#discount)
+    }
+
+    updateStock() {
+        this.#stock--;
     }
 
 }
--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 import { fetchDummyJSON } from "./dummyjson.js";
+import { Product } from "./Product.js";
 
 let productsData;
 fetchDummyJSON()
   .then(data => {
     productsData = data;
     console.log(productsData);
+    
+    for(let i = 0; i < productsData.length; i++) {
+        const product = new Product(productsData[i]);
+    }
   })
   .catch(error => {
     console.error('Error fetching data:', error);
